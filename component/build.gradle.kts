11a87fee3ceaf6b8c76bf25cedef3cc4ac3f8a13
TheRay0410 <yn041010@gmail.com>
2020-09-06 12:15:39 +0900

    componentモジュールの追加

---
+++ component/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    commonConfig()
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+dependencies {
+    implementation(Dependencies.AndroidX.appCompat)
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,2 @@
 rootProject.name = "cash-flow-correction"
-include(":app", ":data:db", ":data:repository", ":domain:model", ":domain:usecase", ":features:accountbook")
+include(":app", ":component", ":data:db", ":data:repository", ":domain:model", ":domain:usecase", ":features:accountbook")
