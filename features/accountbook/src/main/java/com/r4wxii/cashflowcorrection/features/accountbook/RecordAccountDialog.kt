571c12dd315666e26e0f0c09ed6743792d6f5cca
TheRay0410 <yn041010@gmail.com>
2020-09-27 15:34:40 +0900

    収支情報追加ダイアログをFullScreenDialogに変更

--- features/accountbook/src/main/java/com/r4wxii/cashflowcorrection/features/accountbook/RecordAccountDialog.kt
+++ features/accountbook/src/main/java/com/r4wxii/cashflowcorrection/features/accountbook/RecordAccountDialog.kt
@@ -2,20 +2,48 @@ package com.r4wxii.cashflowcorrection.features.accountbook
 
 import android.os.Bundle
 import android.text.InputType
+import android.view.LayoutInflater
 import android.view.View
-import androidx.fragment.app.Fragment
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.r4wxii.cashflowcorrection.features.accountbook.databinding.DialogRecordAccountBinding
 
-class RecordAccountDialog : Fragment(R.layout.dialog_record_account) {
+class RecordAccountDialog : DialogFragment() {
     private val viewModel: AccountBookViewModel by navGraphViewModels(R.id.account_book_navigation)
     private val datePickerBuilder = MaterialDatePicker.Builder.datePicker()
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.let { dialog ->
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog.window?.setLayout(width, height)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialog)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.dialog_record_account, container, false)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val binding = DialogRecordAccountBinding.bind(view)
         binding.viewModel = viewModel
+
+        binding.toolBar.setupWithNavController(findNavController())
 
         datePickerBuilder.setSelection(MaterialDatePicker.todayInUtcMilliseconds())
         val datePicker = datePickerBuilder.build()
