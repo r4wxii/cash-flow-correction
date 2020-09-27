14791acdf5046e8f5522ec765c039500a65d93e9
TheRay0410 <yn041010@gmail.com>
2020-09-27 12:11:58 +0900

    AppBarにNavigationUpボタンを表示

--- app/src/main/java/com/r4wxii/cashflowcorrection/MainActivity.kt
+++ app/src/main/java/com/r4wxii/cashflowcorrection/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.r4wxii.cashflowcorrection
 
 import android.os.Bundle
-import com.r4wxii.cashflowcorrection.features.accountbook.AccountBookFragment
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.r4wxii.cashflowcorrection.databinding.ActivityMainBinding
 import dagger.android.support.DaggerAppCompatActivity
 
 class MainActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolBar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        val configuration = AppBarConfiguration(setOf(R.id.account_book))
+        setupActionBarWithNavController(navController, configuration)
     }
+
+    override fun onSupportNavigateUp(): Boolean = findNavController(R.id.fragment_container).navigateUp()
 }
