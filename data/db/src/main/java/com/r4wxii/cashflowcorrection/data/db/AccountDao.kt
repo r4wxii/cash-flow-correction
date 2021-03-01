0f8caf9f31b78526dce8c7d510101fafa2fc1701
r4wxii <yn041010@gmail.com>
2021-03-01 23:36:10 +0900

    1日に値を取得できないクエリになっていたのを修正

--- data/db/src/main/java/com/r4wxii/cashflowcorrection/data/db/AccountDao.kt
+++ data/db/src/main/java/com/r4wxii/cashflowcorrection/data/db/AccountDao.kt
@@ -11,7 +11,7 @@ interface AccountDao {
     @Query("SELECT * FROM account WHERE id = :id")
     suspend fun getAccount(id: Int): AccountEntity
 
-    @Query("SELECT * FROM account WHERE date(date)>date(CURRENT_DATE, 'start of month')")
+    @Query("SELECT * FROM account WHERE date(date)>=date(CURRENT_DATE, 'start of month')")
     fun getThisMonthAccounts(): Flow<List<AccountEntity>>
 
     @Delete
